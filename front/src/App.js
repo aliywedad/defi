b57da0c80994530dec473b18651d11bfcc54e38c
aliywedad <aliysidahmedwedad@gmail.com>
2024-04-13 14:56:45 -1200

    'g'

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar';
+import logo from './logo.svg';
+// import './App.css';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
