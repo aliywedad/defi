030a34792dbb8ed709220cf748bea6a0e36696aa
AYMAN <126827455+AymanMady@users.noreply.github.com>
2024-04-14 12:16:59 +0000

    ayman

--- back/myapp/urls.py
+++ back/myapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('list_Etudiant/', views.list_Etudiant, name='list_Etudiant'),
     path('list_Admin/', views.list_Admin, name='list_Admin'),
     path('list_Jury/', views.list_Jury, name='list_Jury'),
+    path('list_Equipe/', views.list_Equipe, name='list_Equipe'),
 
     # path('auth', views.isUser, name='isUser'),  
     # path('verifEmail/', views.verification_Email, name='verification_Email'),  
