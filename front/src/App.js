0dd81d7b09a6cad25097304637b7e9b295c064d4
aliywedad <aliysidahmedwedad@gmail.com>
2024-04-13 22:45:57 -1200

    secons one

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import './App.css';
 import { Button } from 'react-bootstrap';
-<<<<<<< HEAD
 import Login from './pages/login';
 import Register from './pages/register';
-=======
 import EtuduentPage from './pages/EtudiantPage';
 import Appp from './components/test';
 import JeryPage from './pages/JeryPage';
 import AdminPage from './pages/AdminPage';
->>>>>>> 6108a34c7fdb38f39e77dae646a537a7035a3b12
 
 
 function App() {
@@ -18,22 +15,20 @@ function App() {
 <>
 {/* <Appp/> */}
     {/* <NavBar/> */}
-<<<<<<< HEAD
     {/* <SidebarExample/> */}
     {/* <Login/> */}
-    <Register/>
-=======
+    {/* <Register/> */}
     {/* <SidebarExample prop={<Etuduent/>}/> */}
     <BrowserRouter>
     <Routes>
-    <Route path="/" element={<EtuduentPage />} /> 
+    <Route path="/" element={<Login/>} /> 
+    <Route path="/Etudiant" element={<EtuduentPage />} /> 
     <Route path="/Jery" element={<JeryPage />} /> 
     <Route path="/Admin" element={<AdminPage />} /> 
 
        </Routes>
     </BrowserRouter>
 
->>>>>>> 6108a34c7fdb38f39e77dae646a537a7035a3b12
 </>
 
   );
